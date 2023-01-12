3d2ad08e73e0fe93e8958d419101d6b9bae96091
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2023-01-12 00:27:50 -0500

    added home fragment and navigation

---
+++ app/src/main/java/com/example/eventive/ui/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.eventive.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.eventive.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/eventive/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.eventive.utils
+
+const val CLIENT_SECRET = "83d65ef799c8c63ed2af10b450b8f70cc930a738b7d638239267b0f8f423fe53"
+const val CLIENT_ID = "MzE0NTMwOTh8MTY3MzQ5OTk3MC4xNTkxNTI3"
